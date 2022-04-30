9a690b197e3b018f1452ba5035d32dc8f750d62e
Egor Grishechko <egorgrishechko@gmail.com>
2022-04-30 09:44:37 +0200

    Update bot.py
    Debug -> Info

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from telegram.ext import (
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
